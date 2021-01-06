533b6708a4826545664a852390ca086878a52623
Thomas-Scherrer <thomas.scherrer@etu.univ-cotedazur.fr>
2021-01-06 13:23:15 +0100

    Exo4 - TouchEvent

---
+++ TD1/Exo4/script.js
@@ -0,0 +1,13 @@
+document.getElementById('touch').addEventListener('touchstart', function(e) {
+
+    if(e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel'){
+        var evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
+        var touch = evt.touches[0] || evt.changedTouches[0];
+        x = touch.pageX;
+        y = touch.pageY;
+        document.write("Coordonnées du toucher: <br>")
+        document.write("Abscisse: " + x + "<br>");
+        document.write("Ordonnée: " + y + "<br>");
+    }
+
+}, false);
