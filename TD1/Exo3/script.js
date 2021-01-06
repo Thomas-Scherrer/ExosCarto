783b8a1b8ca3c76128f238e0b19d78380d7cea65
Thomas-Scherrer <thomas.scherrer@etu.univ-cotedazur.fr>
2021-01-06 11:31:34 +0100

    alpha, beta, gamma added

--- TD1/Exo3/script.js
+++ TD1/Exo3/script.js
@@ -1,3 +1,10 @@
+function process(event) {
+  var alpha = event.alpha;
+  var beta = event.beta;
+  var gamma = event.gamma;
+  document.getElementById("log").innerHTML = "<ul><li>Alpha : " + alpha + "</li><li>Beta : " + beta + "</li><li>Gamma : " + gamma + "</li></ul>"; 
+}
+
 if(window.DeviceOrientationEvent) {
     window.addEventListener("deviceorientation", process, false);
   } else {
