4846207591e678e521ec1943512c1469f93c1699
Thomas-Scherrer <thomas.scherrer@etu.univ-cotedazur.fr>
2021-01-06 11:50:30 +0100

    deviceMotion

--- TD1/Exo3/script.js
+++ TD1/Exo3/script.js
@@ -1,12 +1,25 @@
-function process(event) {
-  var alpha = event.alpha;
-  var beta = event.beta;
-  var gamma = event.gamma;
-  document.getElementById("log").innerHTML = "<ul><li>Alpha : " + alpha + "</li><li>Beta : " + beta + "</li><li>Gamma : " + gamma + "</li></ul>"; 
+function deviceOrientation(orientation) {
+  var alpha = orientation.alpha;
+  var beta = orientation.beta;
+  var gamma = orientation.gamma;
+  document.getElementById("orientation").innerHTML = "<ul><li>Alpha : " + alpha + "</li><li>Beta : " + beta + "</li><li>Gamma : " + gamma + "</li></ul>"; 
+}
+
+function deviceMotion(mouvement) {
+  var x = mouvement.accelerationIncludingGravity.x;
+  var y = mouvement.accelerationIncludingGravity.y;
+  var z = mouvement.accelerationIncludingGravity.z;
+  document.getElementById("mouvement").innerHTML = "<ul><li>X : " + x + "</li><li>Y : " + y + "</li><li>Z : " + z + "</li></ul>"; 
 }
 
 if(window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", process, false);
-  } else {
-    console.log("Le navigateur ne supporte pas l'évènement deviceorientation")
-  }
+  window.addEventListener("deviceorientation", deviceOrientation, false);
+} else {
+  // Le navigateur ne supporte pas l'événement deviceorientation
+}
+
+if(window.DeviceMotionEvent) {
+  window.addEventListener("devicemotion", deviceMotion, false);
+} else {
+  // Le navigateur ne supporte pas l'événement devicemotion
+}
