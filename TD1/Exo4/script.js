9d85d323265e65e2a853ff78f2c0cb1af92a2d7a
Thomas-Scherrer <thomas.scherrer@etu.univ-cotedazur.fr>
2021-01-06 13:30:39 +0100

    touch type

--- TD1/Exo4/script.js
+++ TD1/Exo4/script.js
@@ -1,13 +1,22 @@
-document.getElementById('touch').addEventListener('touchstart', function(e) {
-
-    if(e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel'){
-        var evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
-        var touch = evt.touches[0] || evt.changedTouches[0];
-        x = touch.pageX;
-        y = touch.pageY;
-        document.write("Coordonnées du toucher: <br>")
-        document.write("Abscisse: " + x + "<br>");
-        document.write("Ordonnée: " + y + "<br>");
+document.getElementById("touch").addEventListener(
+  "touchstart",
+  function (e) {
+    if (
+      e.type == "touchstart" ||
+      e.type == "touchmove" ||
+      e.type == "touchend" ||
+      e.type == "touchcancel"
+    ) {
+      var evt = typeof e.originalEvent === "undefined" ? e : e.originalEvent;
+      var touch = evt.touches[0] || evt.changedTouches[0];
+      x = touch.pageX;
+      y = touch.pageY;
+      document.write("Coordonnées du toucher: <br>");
+      document.write("Abscisse: " + x + "<br>");
+      document.write("Ordonnée: " + y + "<br>");
+      document.write("<br>Type du toucher: <br>");
+      document.write(e.type);
     }
-
-}, false);
+  },
+  false
+);
