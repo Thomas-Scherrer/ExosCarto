d92433f9a9636288418f9af4e626460984726d52
Thomas-Scherrer <thomas.scherrer@etu.univ-cotedazur.fr>
2021-01-26 09:11:12 +0100

    TD3/Exo1/ Canvas HTML

--- TD3/Exo1/script.js
+++ TD3/Exo1/script.js
@@ -1,12 +1,32 @@
 function draw() {
   var ctx = document.getElementById("canvas").getContext("2d");
-  var img = new Image();
-  img.onload = function () {
-    for (var i = 0; i < 1; i++) {
-      for (var j = 0; j < 1; j++) {
-        ctx.drawImage(img, j * 1246, i * 333, 1246, 333);
-      }
-    }
-  };
-  img.src = "./images/montagne.jpg";
+  var myimg = document.getElementById("imgMontagne");
+  
+  // Montagne
+  ctx.drawImage(myimg, 0, 0);
+
+  // Maison (carré)
+  ctx.fillStyle = "orange";
+  ctx.fillRect(20, 220, 100, 100);
+
+  // Porte (rectangle)
+  ctx.fillStyle = "brown";
+  ctx.fillRect(30, 270, 30, 50);
+
+  // Toit (triangle)
+  ctx.beginPath();
+  ctx.moveTo(70, 120);
+  ctx.lineTo(20, 220);
+  ctx.lineTo(120, 220);
+  ctx.closePath();
+  ctx.fillStyle = "red";
+  ctx.fill();
+
+  // Soleil
+  ctx.beginPath();
+  ctx.arc(550, 60, 50, 0, Math.PI * 2, true);
+  ctx.strokeStyle = "yellow";
+  ctx.fillStyle = "yellow";
+  ctx.fill();
+  ctx.stroke();
 }
