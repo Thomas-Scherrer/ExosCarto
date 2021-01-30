7d3a6673a2a8cb63a8e7d070022d4657e9973956
Thomas-Scherrer <thomas.scherrer@etu.univ-cotedazur.fr>
2021-01-30 04:14:49 +0100

    change color functions

---
+++ TD3/Exo1.2/script.js
@@ -0,0 +1,9 @@
+// Changement de couleur du ciel au clique du soleil
+function changeColor() {
+    document.getElementById("sky").style.fill = "purple";
+}
+
+// Réinitialisation de la couleur du ciel lorsque l'on double clique sur le soleil
+function reinitSkyColor() {
+    document.getElementById("sky").style.fill = "#0080ff";
+}
