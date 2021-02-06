b4625ed78963f53e9e61a49794322fe3ca694c60
Thomas-Scherrer <thomas.scherrer@etu.univ-cotedazur.fr>
2021-02-06 11:58:25 +0100

    Exo3 finished

--- TD3/Exo3/js/script.js
+++ TD3/Exo3/js/script.js
@@ -5,119 +5,114 @@ const scene = new THREE.Scene();
 
 // init camera
 const camera = new THREE.PerspectiveCamera(
-  75,
+  30,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
-// particules
-/*var particules = new THREE.Particle( new THREE.ParticleCanvasMaterial({
-  color: 0xFF0000,
-  opacity: 1,
-  program: function(context){
-    context.beginPath();
-    context.arc(0,0,1,0,Math.PI * 2, true);
-    context.closePath();
-    context.fill();
-  }
-}));
-particules.position.x = 0;
-particules.position.y = 0;
-particules.position.z = 0;
-particules.scale.x = particules.scale.y = 100;
-scene.add(particules); */
-
 // renderer
 const renderer = new THREE.WebGLRenderer();
-
-// orbit controls
-const orbitControls = new THREE.OrbitControls( camera, renderer.domElement );
-
-// deviceOrientation controls
-const deviceOrientationControls = new THREE.DeviceOrientationControls( camera );
-
-window.addEventListener('devicemotion', function(event) {
-  console.log('Device Motion alpha :' + event.rotationRate.alpha);
-  console.log('Device Motion beta :' + event.rotationRate.beta);
-  console.log('Device Motion gamma :' + event.rotationRate.gamma);
-});
-
-if (window.DeviceOrientationEvent) {
-  window.addEventListener("deviceorientation", function (event) {
-    var rotateDegrees = event.alpha;
-    var frontToBack = event.beta;
-    var leftToRight = event.gamma;
-
-    handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-  }, true);
-}
-
-var handleOrientationEvent = function (rotateDegrees, frontToBack, leftToRight) {
-  console.log('Device Orientation alpha :' + rotateDegrees);  
-  console.log('Device Orientation beta :' + frontToBack); 
-  console.log('Device Orientation gamma :' + leftToRight);
-};
-
-window.addEventListener("compassneedscalibration", function(event) {
-  alert('Your compass needs calibrating! Wave your device in a figure-eight motion');
-  event.preventDefault();
-}, true);
-
-// geometry
-const geometry = new THREE.SphereGeometry( 1, 32, 32 );
-
-// texture loader
-const textureLoader = new THREE.TextureLoader();
-
-// texture
-const material = new THREE.MeshBasicMaterial({
-  map: textureLoader.load('textures/crash.png'),
-});
-
-// sphere
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
-sphere.position.set(1, -1, 0);
-
-// position de la caméra
-camera.position.z = 8;
-//camera.lookAt(particules);
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// GLTF loader instantiation
+// sphere
+const geometry = new THREE.SphereGeometry(1, 32, 32);
+const texture = new THREE.TextureLoader().load("textures/earth-map.png");
+const materialTex = new THREE.MeshBasicMaterial({ map: texture });
+const sphere = new THREE.Mesh(geometry, materialTex);
+
+// orbit controls
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+// white directional light shining from the top
+const directionalLight = new THREE.DirectionalLight(0xffffff, 4);
+
+camera.position.z = 5;
+
+scene.add(sphere);
+scene.add(directionalLight);
+
+// getCurrentPosition
+navigator.geolocation.getCurrentPosition(success, error);
+
+function success(position) {
+    localStorage.setItem('long', position.coords.longitude)
+    localStorage.setItem('lat', position.coords.latitude)
+}
+
+function error() {
+    console.log("Vous n'avez pas autorisé la géolocalisation")
+}
+
+// marqueur à la position actuelle
+var long = localStorage.getItem("long");
+var lat = localStorage.getItem("lat");
+
 const gltfLoader = new THREE.GLTFLoader();
 
-// Load a glTF resource
+// fonction Lat/Lon to cartésien
+function latLonToCartesien(long, lat) {
+  long = (-long * Math.PI) / 180;
+  lat = (lat * Math.PI) / 180;
+  const radius = 1;
+  var x = radius * Math.cos(lat) * Math.cos(long);
+  var y = radius * Math.sin(lat);
+  var z = radius * Math.cos(lat) * Math.sin(long);
+  return {
+    x,
+    y,
+    z,
+  };
+}
+
+// utilisation du canard (duck.gltf) en guise de marqueur à la position actuelle
 gltfLoader.load(
-  // resource 
-  'models/duck.gltf',
-  // called when the resource is loaded
-  function ( gltf ) {
-    scene.add( gltf.scene );
-  },
-
-  // called while loading is progressing
-  function ( xhr ) {
-    console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-  },
-
-  // called when loading has errors
-  function ( error ) {
-    console.log( 'An error happened' );
+  "models/duck.gltf",
+  function (gltf) {
+    gltf.scene.scale.set(0.02, 0.02, 0.02);
+    var coords = latLonToCartesien(long, lat);
+    gltf.scene.position.x = coords.x;
+    gltf.scene.position.y = coords.y;
+    gltf.scene.position.z = coords.z;
+    scene.add(gltf.scene);
   }
 );
 
-// White directional light shining from the top
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 4 );
-scene.add( directionalLight );
+// marqueurs sous forme de spheres avec les drapeaux en texture pour identifier les pays
+function sphereMarqueur(drapeaux) {
+  const geometry = new THREE.SphereGeometry(0.01, 32, 32);
+  const texture = new THREE.TextureLoader().load(drapeaux);
+  const materialTex = new THREE.MeshBasicMaterial({ map: texture });
+  const sphere = new THREE.Mesh(geometry, materialTex);
+  return sphere;
+}
+
+// fonction utilisant l'API restcountries
+function getCountries(callback) {
+  fetch("https://restcountries.eu/rest/v2/all").then((resp) => {
+    resp.json().then((data) => {
+      callback(data);
+    });
+  });
+}
+
+// fonction ajoutant les marqueurs aux emplacements des différents pays
+function marqueurs(texture, long, lat) {
+  var marqueur = sphereMarqueur(texture);
+  var coords = latLonToCartesien(long, lat);
+  marqueur.position.set(coords.x, coords.y, coords.z);
+  scene.add(marqueur);
+}
+
+getCountries(function (countries) {
+  countries.forEach((country) => {
+    marqueurs(country.flag, country.latlng[1], country.latlng[0]);
+  });
+});
 
 function animate() {
   requestAnimationFrame(animate);
-  sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 animate();
