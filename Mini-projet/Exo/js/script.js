fb235c76a0e216d447e168391856b9fdc3808d3f
Thomas-Scherrer <thomas.scherrer@etu.univ-cotedazur.fr>
2021-02-11 11:16:02 +0100

    threeJS

--- Mini-projet/Exo/js/script.js
+++ Mini-projet/Exo/js/script.js
@@ -5,12 +5,13 @@ var axeX;
 function init() {
   imgBoussole = document.getElementById("imgBoussole");
   imgAiguille = document.getElementById("imgAiguille");
-  window.requestAnimationFrame(draw1);
-  window.requestAnimationFrame(draw2);
+  // Canvas
+  window.requestAnimationFrame(draw);
 }
 
-function draw1() {
-  var ctx1 = document.getElementById("canvas1").getContext("2d");
+// Canvas
+function draw() {
+  var ctx1 = document.getElementById("canvas").getContext("2d");
 
   ctx1.clearRect(0, 0, 300, 300);
 
@@ -24,44 +25,7 @@ function draw1() {
   ctx1.drawImage(imgAiguille, -301, -145);
   ctx1.restore();
 
-  window.requestAnimationFrame(draw1);
-}
-
-function draw2() {
-  var ctx2 = document.getElementById('canvas2').getContext('2d');
-
-  ctx2.clearRect(0, 0, 300, 300);
-
-  ctx2.save();
-  ctx2.translate(400, 150);
-  ctx2.beginPath();
-  ctx2.lineWidth = 14;
-  ctx2.strokeStyle = 'blue';
-  ctx2.arc(0, 0, 142, 0, Math.PI*2, true);
-  ctx2.stroke();
-  ctx2.restore();
-
-  ctx2.save();
-  ctx2.translate(400, 150);
-  ctx2.rotate(axeX*(Math.PI/180));
-  ctx2.strokeStyle = "red";
-  ctx2.fillStyle = "red";
-  ctx2.lineWidth = 6;
-  ctx2.beginPath();
-  ctx2.moveTo(-100, 0);
-  ctx2.lineTo(100, 0);
-  ctx2.stroke();
-  ctx2.restore();
-
-  window.requestAnimationFrame(draw2);
-}
-
-function deviceOrientation(event){
-  axeX = event.alpha;
-}
-
-if (window.DeviceOrientationEvent) {
-  window.addEventListener('deviceorientation', deviceOrientation, false);
+  window.requestAnimationFrame(draw);
 }
 
 init();
---
+++ Mini-projet/Exo/js/threejs.js
@@ -0,0 +1,31 @@
+// Three JS
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.ConeGeometry( 5, 20, 32 );
+const texture = new THREE.TextureLoader().load( '../images/earth-map.png' );
+const materialTex = new THREE.MeshBasicMaterial( { map: texture } );
+const cone = new THREE.Mesh( geometry, materialTex );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 100 );
+
+const animate = function () {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+};
+
+camera.position.z = 50;
+
+scene.add(cone);
+scene.add(directionalLight);
+
+renderer.render( scene, camera );
+
+animate();
+
+function rotateCone(degrees) {
+    cone.rotation.z = THREE.Math.degToRad(-degrees);
+}
